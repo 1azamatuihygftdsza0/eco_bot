c6dac581bee4c0693f623c96cae84267ff1e283c
1azamatuihygftdsza0 <azamatzoyirov2@gmail.com>
2024-12-04 10:26:26 +0500

    uyga vazifa
    uyga vazifa

---
+++ uyga vazifa 3.2.py
@@ -0,0 +1,14 @@
+import math
+a=int(input("a sonini kiriting = "))
+b=int(input("b sonini kiriting = "))
+c=input("amalni tanlang = ")
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=='/':
+    print(a/b)
+else:
+    print("bunday amal yo'q")
